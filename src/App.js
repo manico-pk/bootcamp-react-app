9a40f15356bf7caa2c9a1ebbbc84363dcd2aebde
manico-pk <62180781+manico-pk@users.noreply.github.com>
2021-01-07 04:59:52 +0500

    final

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <P>MUHAMMAD USMAN</P>
+        <p>MUHAMMAD USMAN</p>
         <a
           className="App-link"
           href="https://reactjs.org"
